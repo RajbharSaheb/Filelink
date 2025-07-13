483fe9c6b497f5e6c1291642eef1d464229da774
RajbharSaheb <110810360+RajbharSaheb@users.noreply.github.com>
2025-07-13 14:12:48 +0530

    Delete FileStream directory

--- FileStream/bot.py
+++
@@ -1,8 +0,0 @@
-from pyrogram import Client
-from FileStream.config import API_ID, API_HASH, BOT_TOKEN
-
-app = Client("filestreambot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-
-def run_bot():
-    from FileStream.handlers import upload_handler  # ensure handlers are loaded
-    app.run()
