662690fcd618079b55c3cac055bdb40b2a886c85
RajbharSaheb <110810360+RajbharSaheb@users.noreply.github.com>
2025-07-13 14:49:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,13 @@
-from pyrogram import Client, filters
+from dotenv import load_dotenv
 import os
 
-app = Client("my_bot", bot_token=os.getenv("BOT_TOKEN"), api_id=12345, api_hash="yourhash")
+load_dotenv()
+
+api_id = int(os.getenv("API_ID"))      # int hona chahiye
+api_hash = os.getenv("API_HASH")
+bot_token = os.getenv("BOT_TOKEN")
+
+app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 @app.on_message(filters.document | filters.video | filters.audio)
 async def save_file(client, message):
